b575f1e1ffd15674ba0fd27764da5586e29b6969
Aleksandr Pavlov <sanek_pavlov03@mail.ru>
2024-12-10 14:27:48 +0400

    add analysis in report change api url to dev

---
+++ src/models/Analysis.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Vkrsmart\Sdk\Models;
+
+class Analysis extends Model
+{
+    protected string $prefix = 'report';
+
+
+    /**
+     * Получить report по id
+     * @param int $id
+     * @param array $params
+     * @return bool
+     */
+    public function get(int $id, array $params = []): bool
+    {
+        if ($this->master) {
+            $apiMethod = "/master/" . $this->prefix . "/{$id}/analysis";
+        } else {
+            $apiMethod = "/" . $this->prefix . "/{$id}/analysis";
+        }
+        $this->response = $this->getClient()->makeRequest($apiMethod, $params);
+        return $this->getSuccess();
+    }
+
+    /**
+     * @return false|mixed
+     */
+    public function getAnalysis(): mixed
+    {
+        return $this->getValue('analysis');
+    }
+
+}
