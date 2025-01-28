a3b28bf331cb1f6fecd7d128b384de294a4ecde8
Aleksandr Pavlov <sanek_pavlov03@mail.ru>
2025-01-28 17:19:55 +0400

    fix api url

--- src/core/Curl.php
+++ src/core/Curl.php
@@ -7,7 +7,7 @@ use Exception;
 use Illuminate\Support\Facades\Log;
 class Curl
 {
-    const API = 'https://api.bse.dev.vkr-smart.ru/master';
+    const API = 'https://api.bse.dev.vkr-smart.ru';
 
 
     /**
